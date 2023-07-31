66f2946ace0e763c86e7bfa38c46c53c6d0eefac
divyesh2456 <divyesh.k.dubey@kipi.bi>
2023-07-31 19:28:00 +0530

    latest

--- migrations/V2.1.12__demo_table_alter.sql
+++ migrations/V2.1.12__demo_table_alter.sql
@@ -1,2 +1,6 @@
-use SCHEMA raw
-create table t2 as select * from DEMO_DB.PROCESSED.TABLE2 a join DEMO_DB.RAW.TABLE1 b on a.FIRST_NAME = b.FIRST_NAME;
+USE SCHEMA DEMO;
+CREATE TABLE TestTableAb
+(
+   FIRST_NAME VARCHAR
+  ,LAST_NAME VARCHAR
+);
